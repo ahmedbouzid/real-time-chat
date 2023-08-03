9573a0de96df9874defadf63de0f4b2abf04ed4a
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-03 20:13:47 +0200

    configure type orm && cnfirm db connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+  import { Module } from '@nestjs/common';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
+  import { ConfigModule } from '@nestjs/config';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+  import * as path from 'path';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'mysql', // Update this line
+        username: 'root',
+        // url: process.env.DATABASE_URL, // Comment out or remove this line
+        database: 'chat',
+        password: 'ahmed', // Replace with the actual password if required
+        autoLoadEntities: true,
+        entities: [path.join(process.cwd(),'dist/**/*.entity.js')],
+        synchronize: true,
+      }),
+      UserModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
