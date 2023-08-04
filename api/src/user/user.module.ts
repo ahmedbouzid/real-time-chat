8e3434b1b53f54f09d0a5ed8a127daa95263969e
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-04 19:36:31 +0200

    app module config

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './controller/user/user.controller';
+import { UserService } from './service/user/user.service';
+import { TypeOrmModule} from '@nestjs/typeorm'
+import { UserEntity } from './model/user.entity';
+import { UserHeplerService } from './service/user-hepler/user-hepler.service';
 
-@Module({})
+@Module({
+  imports:[
+    TypeOrmModule.forFeature([UserEntity])
+  ],
+  controllers: [UserController],
+  providers: [UserService , UserHeplerService],
+  exports: [UserService]
+})
 export class UserModule {}
