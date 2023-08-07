da8dc426e494d69abb86b70aa080c715935b34e5
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-08 01:09:26 +0200

    jwt module injection in auth module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService] , 
+  imports : [
+    JwtModule.registerAsync({
+      imports : [ConfigModule],
+      inject: [ConfigService] , 
+      useFactory : async(configService : ConfigService) => ({
+        secret : configService.get('JWT_SECRET') ,
+        signOptions: {expiresIn : '355123s'}
+      })
+    })
+  ],
+  providers: [AuthService , JwtStrategy , JwtStrategy] , 
   exports : [AuthService]
 })
 export class AuthModule {}
