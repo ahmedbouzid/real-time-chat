c21bbe8aaf1ab78f9a9ac17c0cb16d8527339e13
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-08 11:44:02 +0200

    routing for public module

--- chatfrontend/src/app/public/public-routing.module.ts
+++ chatfrontend/src/app/public/public-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'login' , component:LoginComponent} ,
+  {path :'registre' , component :RegisterComponent} ,
+  {path :'**' , redirectTo :'login' , pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
