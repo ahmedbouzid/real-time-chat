7b85ebc101d94bd5846e3013f5905ce20b3d5876
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-08 11:40:33 +0200

    routing module

--- chatfrontend/src/app/app-routing.module.ts
+++ chatfrontend/src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
 {
   path:'public' ,
   loadChildren:() => import ('./public/public.module').then(m => m.PublicModule)
+} ,
+{
+  path:'**',
+  redirectTo:'public' ,
+pathMatch : 'full'
 }
 ];
 
