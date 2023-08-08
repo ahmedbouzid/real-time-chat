a01041d07e10ef66aea5079892554dd05a0b9550
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-09 00:12:23 +0200

    create user service

---
+++ chatfrontend/src/app/public/services/user-service/user-service.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { UserI } from 'src/app/model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor(
+    private http : HttpClient ,
+    private snackbar : MatSnackBar) { }
+    create (user : UserI) : Observable <UserI> {
+      return this.http.post('api/user' , user).pipe (
+        tap((createUser : UserI) => this.snackbar.open(`User ${createUser.username} create succufully` , `close` ,{
+          duration :2000 , horizontalPosition:'right' , verticalPosition:'top'
+        } )),
+        catchError (e => {
+          this.snackbar.open(`user could not be created to ${e.error.message}` ,
+          `close` , {
+            duration : 5000 , horizontalPosition:'right' , verticalPosition:'top'
+          }
+          )
+          return throwError(e)
+        })
+      )
+    }
+}
