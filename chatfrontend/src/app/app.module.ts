9ff0b016f683bbbf1eff7651c1f6c0c2e7e19f72
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-09 00:12:42 +0200

    create login component

--- chatfrontend/src/app/app.module.ts
+++ chatfrontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+/// Angular Material
+
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule ,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
