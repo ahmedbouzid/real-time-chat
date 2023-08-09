b0efae3ab083a6fb70b8b4f4d750db254c726fbf
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-10 01:15:50 +0200

    app module setup middelware

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,12 @@
-  import { Module } from '@nestjs/common';
   import { AppController } from './app.controller';
   import { AppService } from './app.service';
   import { ConfigModule } from '@nestjs/config';
   import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { UserHeplerService } from './user/service/user-hepler/user-hepler.service';
 import { AuthModule } from './auth/auth.module';
   import * as path from 'path';
-
+  import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+   import { AuthMiddleware } from './middleware/auth.middleware';  // Use lowercase 'auth.middleware'
   @Module({
     imports: [
       ConfigModule.forRoot({ isGlobal: true }),
@@ -28,4 +27,15 @@ import { AuthModule } from './auth/auth.module';
     controllers: [AppController],
     providers: [AppService],
   })
-  export class AppModule {}
+  export class AppModule  implements NestModule  {
+    configure(consumer: MiddlewareConsumer) {
+      consumer
+        .apply(AuthMiddleware)
+        .exclude(
+          { path: '/api/user', method: RequestMethod.POST},
+          {path: '/api/user/login', method: RequestMethod.POST}
+        )
+        .forRoutes('')
+    }
+  
+}
