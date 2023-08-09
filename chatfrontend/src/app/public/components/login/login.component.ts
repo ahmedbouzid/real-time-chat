649656884dc05c043e3a349638271e8f1d921da7
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-09 11:50:05 +0200

    create login interface and   inject dependencies

--- chatfrontend/src/app/public/components/login/login.component.ts
+++ chatfrontend/src/app/public/components/login/login.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth-service/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  loginForm !: FormGroup ;
-constructor(private fb : FormBuilder) {}
 
-  ngOnInit(): void {
-    
+
+constructor( private authService : AuthService , private router : Router) {}
+
+
+  loginForm : FormGroup = new FormGroup ({
+    email : new FormControl(null , [Validators.required , Validators.email]) ,
+    password : new FormControl(null , [Validators.required]),
+
+  })
+
+  get email (): FormControl{
+    return this.loginForm.get('email') as FormControl
   }
-
-
+  get password () : FormControl {
+    return this.loginForm.get('password') as FormControl
+  }
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value)
+      this.authService.login({
+        email : this.email.value ,
+        password : this.password.value
+      }).pipe(
+        tap(() => this.router.navigate(['../../private/dashboard']))
+      ).subscribe()
+    }
+  }
 }
