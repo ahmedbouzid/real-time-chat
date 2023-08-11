82470593bc786570134de5a583fddd055e3da205
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-11 12:56:35 +0200

    create chate interface (front end)

--- chatfrontend/src/app/private/services/chat.service.ts
+++ chatfrontend/src/app/private/services/chat.service.ts
@@ -1,5 +1,8 @@
+// chat.service.ts
 import { Injectable } from '@angular/core';
 import { CustomSocket } from '../sokets/custom-socket';
+import { RoomI, RoomPaginateI } from 'src/app/model/room.interface';
+import { UserI } from 'src/app/model/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,27 @@ export class ChatService {
   constructor(private socket: CustomSocket) { }
 
   sendMessage() {
+    
   }
 
   getMessage() {
     return this.socket.fromEvent('message');
+  }
+
+  getMyRooms() {
+    return this.socket.fromEvent<RoomPaginateI>('rooms');
+  }
+
+  createRoom() {
+    const user2: UserI = {
+      id: 1
+    };
+
+    const room: RoomI = {
+      name: 'Testroom',
+      users: [user2]
+    }
+
+    this.socket.emit('createRoom', room);
   }
 }
