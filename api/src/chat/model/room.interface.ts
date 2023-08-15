c08e7cc5e6a63dbe74b4a5596c11d8351e5394c1
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-15 16:09:59 +0200

    room interface

--- api/src/chat/model/room.interface.ts
+++ api/src/chat/model/room.interface.ts
@@ -2,11 +2,10 @@ import { UserI } from "src/user/model/user.interface";
 
 
 export interface RoomI {
-
-    id ?: number ; 
-    name ?: string ; 
-    description ?: string ; 
-    users ?: UserI[] ; 
-    createdAt ?: Date ; 
-    updatedAt ?: Date ;
-}
+    id?: number;
+    name?: string;
+    description?: string;
+    users?: UserI[];
+    created_at?: Date;
+    updated_at?: Date;
+  }
