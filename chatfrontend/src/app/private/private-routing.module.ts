4cb4ba3e93b17aa01f40df2d426976381e1f4f97
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-15 16:12:02 +0200

    add create room path in private routing module

--- chatfrontend/src/app/private/private-routing.module.ts
+++ chatfrontend/src/app/private/private-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
 const routes: Routes = [
   {path:'dashboard' , component :DashboardComponent} ,
+  {
+    path: 'create-room',component: CreateRoomComponent},
   {path :'**' , redirectTo:'dashboard' , pathMatch:'full'}
 ];
 
