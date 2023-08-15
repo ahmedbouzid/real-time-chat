65063641b8cc5f913d7a37ca11585d236a4d6a44
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-15 16:10:31 +0200

    impliment room service

--- api/src/chat/service/room-service/room/room.service.ts
+++ api/src/chat/service/room-service/room/room.service.ts
@@ -9,40 +9,32 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class RoomService {
-    constructor(
-    @InjectRepository(RoomEntity) 
-    private readonly roomRepo :  Repository<RoomEntity>
-    ) {}
-     
-    async createRoom(room: RoomI, creator: UserI): Promise<RoomI> {
-      const newRoom = await this.addCreatorToRoom(room, creator);
-      return this.roomRepo.save(newRoom);
-    }
-  
-    async getRoomsForUser(userId: number, options: IPaginationOptions): Promise<Pagination<RoomI>> {
-      const query = this.roomRepo
-      .createQueryBuilder('room')
-      .leftJoin('room.users', 'user')
-      .where('user.id = :userId', {userId})
-  
-      return paginate(query, options);
-    }
-  
-    async addCreatorToRoom(room: RoomI, creator: UserI): Promise<RoomI> {
-      room.users.push(creator);
-      return room;
-    }
-   
-  
-    async getRoomById(roomId: number): Promise<RoomI | undefined> {
-      return this.roomRepo
-        .createQueryBuilder('room')
-        .leftJoinAndSelect('room.users', 'users')
-        .where('room.id = :roomId', { roomId })
-        .getOne();
-    }
-  
-    async saveRoom(room: RoomI): Promise<RoomI> {
-      return this.roomRepo.save(room);
-    }
+
+
+  constructor(
+    @InjectRepository(RoomEntity)
+    private readonly roomRepository: Repository<RoomEntity>
+  ) { }
+
+  async createRoom(room: RoomI, creator: UserI): Promise<RoomI> {
+    const newRoom = await this.addCreatorToRoom(room, creator);
+    return this.roomRepository.save(newRoom);
+  }
+
+  async getRoomsForUser(userId: number, options: IPaginationOptions): Promise<Pagination<RoomI>> {
+    const query = this.roomRepository
+    .createQueryBuilder('room')
+      .leftJoin('room.users', 'users')
+      .where('users.id = :userId', { userId })
+      .leftJoinAndSelect('room.users', 'all_users')
+      .orderBy('room.updated_at', 'DESC');
+
+    return paginate(query, options);
+  }
+
+  async addCreatorToRoom(room: RoomI, creator: UserI): Promise<RoomI> {
+    room.users.push(creator);
+    return room;
+  }
+
 }
