69d39f5e06ca544410a4ad22aa7eb0daafbed0f2
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-21 12:19:11 +0200

    chat room refactor

--- chatfrontend/src/app/private/components/chat-room/chat-room.component.ts
+++ chatfrontend/src/app/private/components/chat-room/chat-room.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map, startWith, tap } from 'rxjs/operators';
 import { MessagePaginateInterface } from 'src/app/model/message.interface';
 import { RoomI } from 'src/app/model/room.interface';
 import { ChatService } from '../../services/chat.service';
@@ -10,18 +11,23 @@ import { FormControl, Validators } from '@angular/forms';
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss']
 })
-export class ChatRoomComponent implements OnInit, OnChanges, OnDestroy {
+export class ChatRoomComponent implements  OnChanges, OnDestroy ,AfterViewInit  {
 
   @Input() chatRoom: RoomI;
+  @ViewChild('messages') private messagesScroller: ElementRef;
 
-  messages$: Observable<MessagePaginateInterface> = this.chatService.getMessages().pipe(
-    map((messagePaginate : MessagePaginateInterface) => {
-      const items = messagePaginate.items.sort((a , b) => new Date (a.created_at).getTime()- new Date (b.created_at).getTime()) ;
-      messagePaginate.items = items ;
-      return messagePaginate ;
-    })
+  messagesPaginate$: Observable<MessagePaginateInterface> = combineLatest([this.chatService.getMessages(), this.chatService.getAddedMessage().pipe(startWith(null))]).pipe(
+    map(([messagePaginate, message]) => {
+      if (message && message.room.id === this.chatRoom.id && !messagePaginate.items.some(m => m.id === message.id)) {
+        messagePaginate.items.push(message);
+      }
+      const items = messagePaginate.items.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
+      messagePaginate.items = items;
+      return messagePaginate;
+          }) ,
+          tap(() => this.scrollToBottom())
+    )
 
-  );
 
   chatMessage: FormControl = new FormControl(null, [Validators.required]);
 
@@ -36,6 +42,10 @@ export class ChatRoomComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
   ngOnDestroy() {
     this.chatService.leaveRoom(this.chatRoom);
   }
@@ -44,5 +54,9 @@ export class ChatRoomComponent implements OnInit, OnChanges, OnDestroy {
     this.chatService.sendMessage({text: this.chatMessage.value, room: this.chatRoom});
     this.chatMessage.reset();
   }
+  scrollToBottom(): void {
+    setTimeout(() => {this.messagesScroller.nativeElement.scrollTop = this.messagesScroller.nativeElement.scrollHeight}, 1);
+  }
+
 
 }
