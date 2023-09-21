cc1a4db15841e2222d077540d53bd4df67d031ef
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-21 10:35:21 +0200

    chat service logic

--- chatfrontend/src/app/private/services/chat.service.ts
+++ chatfrontend/src/app/private/services/chat.service.ts
@@ -5,22 +5,31 @@ import { RoomI, RoomPaginateI } from 'src/app/model/room.interface';
 import { UserI } from 'src/app/model/user.interface';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
+import { MessageI, MessagePaginateInterface } from 'src/app/model/message.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
   constructor(private socket: CustomSocket , private snackBar : MatSnackBar   ) { }
-  sendMessage() {
-  }
-  getMessage() : Observable<RoomPaginateI> {
-    return this.socket.fromEvent('message');
+  sendMessage(message: MessageI) {
+    this.socket.emit('addMessage', message);
   }
 
+  joinRoom(room: RoomI) {
+    this.socket.emit('joinRoom', room);
+  }
+
+  leaveRoom(room: RoomI) {
+    this.socket.emit('leaveRoom', room);
+  }
+
+  getMessages(): Observable<MessagePaginateInterface> {
+    return this.socket.fromEvent<MessagePaginateInterface>('messages');
+  }
   getMyRooms(): Observable<RoomPaginateI> {
     return this.socket.fromEvent<RoomPaginateI>('rooms');
   }
-
   emitPaginateRooms(limit: number, page: number) {
     this.socket.emit('paginateRooms', {limit, page});
   }
